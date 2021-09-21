c9db775a0d62f0ef837556910e264d466e46d904
Eugene Timoshevsky <eugene.t@ontapgroup.com>
2021-09-21 23:41:14 +0300

    Add Integer and Float methods

--- rfkb/rfkb.go
+++ rfkb/rfkb.go
@@ -4,13 +4,14 @@ package rfkb
 import (
 	"bufio"
 	"log"
+	"strconv"
 	"strings"
 )
 import "os"
 
 // ReadString reads string types of values
 // input ends with a line break
-// deleting '\n' rune and return inputed string. 
+// deleting '\n' rune and return inputted string.
 func ReadString() string {
 	reader := bufio.NewReader(os.Stdin)
 	str, err := reader.ReadString('\n')
@@ -18,4 +19,36 @@ func ReadString() string {
 		log.Fatal(err)
 	}
 	return strings.TrimSpace(str)
+}
+
+// ReadInt reads string types of values, that may be converted to integer
+// input ends with a line break
+// deleting '\n' rune, convert to integer and return integer.
+func ReadInt() int {
+	reader := bufio.NewReader(os.Stdin)
+	i, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	newInt, err := strconv.Atoi(strings.TrimSpace(i))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return newInt
+}
+
+// ReadFloat reads string types of values, that may be converted to float
+// input ends with a line break
+// deleting '\n' rune, convert to integer and return float.
+func ReadFloat() float64 {
+	reader := bufio.NewReader(os.Stdin)
+	f, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFloat, err := strconv.ParseFloat(strings.TrimSpace(f), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return newFloat
 }
